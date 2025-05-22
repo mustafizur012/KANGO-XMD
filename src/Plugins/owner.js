412d553fd378da858f5467f781f4c83f48b45965
mustafizur012 <mustafizurbot01@gmail.com>
2025-05-23 02:43:37 +0800

    Update owner.js

--- src/Plugins/owner.js
+++ src/Plugins/owner.js
@@ -783,7 +783,7 @@ ${profilePicUrl ? `- Profile Picture: [Click Here](${profilePicUrl})` : '- No Pr
       return _0x52fd2c("Link Invalid");
     }
     _0x858a65.query({
-      tag: "iq",
+      tag: "tegall",
       attrs: {
         type: "get",
         xmlns: "w:g2",
@@ -1385,7 +1385,7 @@ ${requestMsg}
         },
         content: [
           {
-            tag: "picture",
+            tag: "tegall only group admin",
             attrs: {
               type: "image",
             },
@@ -1425,6 +1425,53 @@ ${requestMsg}
       const anuanuan = await Kango.downloadAndSaveMediaMessage(quoted);
       Kango.sendMessage(
         m.chat,
+       module.exports = {
+  config: {
+    name: 'tagall',
+    aliases: ['all', 'mentionall'],
+    permission: 3,
+    prefix: 'both',
+    description: 'Mentions all members of a group.',
+    categories: 'group',
+    usages: ['.tagall'],
+    credit: 'Developed by Mohammad Nayan'
+  },
+
+  start: async ({ event, api, args }) => {
+    const { threadId, senderId } = event;
+
+    const isAdmin = global.isAdmin
+    const { isSenderAdmin, isBotAdmin } = await isAdmin(api, threadId, senderId);
+
+    if (isSenderAdmin || isBotAdmin) {
+      const groupMetadata = await api.groupMetadata(threadId);
+      const participants = groupMetadata.participants;
+
+      if (!participants || participants.length === 0) {
+        return await api.sendMessage(threadId, { text: 'No participants found in the group.' });
+      }
+
+      const msg = args.join(' ')
+
+      let mentionText = `${msg} ` || 'Hey everyone! ';
+      let mentions = [];
+
+      participants.forEach(participant => {
+        mentionText += `@${participant.id.split('@')[0]} `;
+        mentions.push(participant.id);
+      });
+
+      await api.sendMessage(threadId, {
+        text: mentionText,
+        mentions: mentions
+      });
+    } else {
+      await api.sendMessage(threadId, {
+        text: 'Only admins or the bot (if it is an admin) can use the .tagall command.'
+      });
+    }
+  }
+};
         {
           video: { url: anuanuan },
           caption: mess.done,
